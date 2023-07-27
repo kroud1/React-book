1ec86f3eeabeac54607afae6cb96ce864928b611
kroud1 <kroud12@gmail.com>
2023-07-27 17:51:10 +0900

    0727

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import React from "react";
 import "./App.css";
-import { Routes, Route,} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Todo from "./views/Todo";
 import User from "./views/User";
-import React from "react";
+import SignUp from "./views/AuthenticationView/SignUpView";
+
 function App() {
   return (
     <>
       <Todo />
       <Routes>
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="/user" element={<User />} />
-
       </Routes>
     </>
   );
--- src/views/AuthenticationView/SignUpView/index.tsx
+++ src/views/AuthenticationView/SignUpView/index.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Box, Card, CardContent, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
 
 export default function SignUP() {
   // 회원가입하는 user의 정보의 State를 하나의 객체로 관리
@@ -36,6 +45,74 @@ export default function SignUP() {
   const emailRegExp = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
   // 이메일 주소 형식의 문자열
 
+  const handleInput = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setUserInfo({ ...userInfo, [name]: value });
+
+    switch (name) {
+      case "userId":
+        setErrors({ ...errors, userId: value === "" || !idRegExp.test(value) });
+        break;
+      case "userPassword":
+        setErrors({ ...errors, userPassword: !pwRegExp.test(value) });
+        break;
+      case "userPasswordCheck":
+        setErrors({
+          ...errors,
+          userPasswordCheck: userInfo.userPassword !== value,
+        });
+        break;
+      case "userEmail":
+        setErrors({
+          ...errors,
+          userEmail: value === "" || !emailRegExp.test(value),
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const signUpHandler = async () => {
+    const {
+      userId,
+      userPassword,
+      userPasswordCheck,
+      userName,
+      userPhone,
+      userEmail,
+      userAddress,
+    } = userInfo;
+    const {
+      userId: idError,
+      userPassword: pwError,
+      userPasswordCheck: pwckError,
+      userEmail: emailError,
+    } = errors;
+
+    if (
+      idError ||
+      pwError ||
+      pwckError ||
+      emailError ||
+      userName === "" ||
+      userPhone === "" ||
+      userAddress === ""
+    ) {
+      alert("모든 조건을 만족시켜주세요.");
+      return;
+    }
+
+    try {
+      await axios.post("http://localhost:4080/api/auth/signUp", userInfo);
+      alert("회원가입 완료! 환영합니다.");
+    } catch (error) {
+      alert("입력 값을 다시 확인해주세요.");
+    }
+  };
+
   return (
     <>
       <Typography variant="h3" paddingTop={"2vw"} textAlign={"center"}>
@@ -50,16 +127,130 @@ export default function SignUP() {
             <TextField
               fullWidth
               label="아이디"
+              name="userId"
               type="id"
               variant="standard"
-              error={idError}
+              error={errors.userId}
               helperText={
-                idError
+                errors.userId
                   ? "영어 대소문자 및 숫자로 4 ~ 8자리 입력하세요."
                   : false
               }
-            ></TextField>
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="비밀번호"
+              name="userPassword"
+              type="password"
+              variant="standard"
+              error={errors.userPassword}
+              helperText={
+                errors.userPassword
+                  ? "영어 대소문자 및 숫자, 특수문자(! @)로 8 ~ 12자리 입력하세요."
+                  : false
+              }
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="비밀번호 체크"
+              name="UserPasswordCheck"
+              type="password"
+              variant="standard"
+              error={errors.userPasswordCheck}
+              helperText={
+                errors.userPasswordCheck
+                  ? "비밀번호가 일치하지 않습니다."
+                  : false
+              }
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="이름"
+              name="userName"
+              type="name"
+              variant="standard"
+              // error={userInfo.userName === "" ? "이름을 입력하세요" : false}
+              helperText={
+                userInfo.userName === "" ? "이름을 입력하세요." : false
+              }
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="전화번호"
+              name="userPhone"
+              type="phone"
+              variant="standard"
+              // error={userInfo.userPhone === "" ? "전화번호를 입력하세요" : false}
+              helperText={
+                userInfo.userPhone === "" ? "전화번호를 입력하세요." : false
+              }
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="이메일"
+              name="userEmail"
+              type="email"
+              variant="standard"
+              error={errors.userEmail}
+              helperText={
+                errors.userEmail ? "이메일 형식에 맞게 작성하여 주세요." : false
+              }
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="주소"
+              name="userAddress"
+              type="address"
+              variant="standard"
+              // error={userInfo.userAddress === "" ? "주소를 입력하세요" : false}
+              helperText={userInfo.userAddress ? "주소를 입력하세요." : false}
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="주소"
+              name="userAddressDetail"
+              type="address"
+              variant="standard"
+              // error={userInfo.userAddressDetail === "" ? "상세주소를 입력하세요" : false}
+              helperText={
+                userInfo.userAddressDetail ? "상세주소를 입력하세요." : false
+              }
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="자녀 생년월일"
+              name="userKidBirth"
+              type="kidBirth"
+              variant="standard"
+              onChange={handleInput}
+            />
+            <TextField
+              fullWidth
+              label="자녀 생년월일"
+              name="recommendedUserId"
+              type="recommendedUserId"
+              variant="standard"
+              onChange={handleInput}
+            />
           </CardContent>
+          <CardActions>
+            <Button
+              fullWidth
+              onClick={() => signUpHandler()}
+              variant="contained"
+              sx={{ bgcolor: "#f0a500" }}
+            >
+              회원 가입
+            </Button>
+          </CardActions>
         </Card>
       </Box>
     </>
