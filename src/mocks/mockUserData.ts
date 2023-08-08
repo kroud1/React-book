e7f71131391125ac4c558ccb34624f70924aa58c
kroud1 <kroud12@gmail.com>
2023-08-08 17:49:15 +0900

    0808

--- src/mocks/mockUserData.ts
+++ src/mocks/mockUserData.ts
@@ -1,5 +1,53 @@
-export const mockUser = {
-  userName: "Seung Ah",
-  userEmail: "qwe123@naver.com",
-  userPhone: "010-1234-5678",
+// export const mockUser = {
+//   userName: "Seung Ah",
+//   userEmail: "qwe123@naver.com",
+//   userPhone: "010-1234-5678",
+// };
+
+type User = {
+  userId: string;
+  userPassword: string;
+  userEmail: string;
+  userAddress: string;
+  userAddressDatail: string;
+  userName: string;
+  userPhone: string;
+  userKidBirth: string;
+  userWithdraw?: string;
 };
+
+const mockUserData: User[] = [
+  {
+    userId: "qwe123",
+    userPassword: "qweq23",
+    userEmail: "23dsf@eqw.wee",
+    userAddress: "부산시",
+    userAddressDatail: "부산진구",
+    userName: "seungah",
+    userPhone: "010-1111-2222",
+    userKidBirth: "2024-03-06",
+
+  },
+  {
+    userId: "qwe1ggg23",
+    userPassword: "qweqsdf23",
+    userEmail: "2235263dsf@eqw.wee",
+    userAddress: "부산시",
+    userAddressDatail: "부산진구",
+    userName: "Bseungah",
+    userPhone: "010-1111-2222",
+    userKidBirth: "2024-03-06",
+  },
+  {
+    userId: "qwe123",
+    userPassword: "qweq23",
+    userEmail: "23dsf@eqw.wee",
+    userAddress: "부산시",
+    userAddressDatail: "부산진구",
+    userName: "Cseungah",
+    userPhone: "010-1111-2222",
+    userKidBirth: "2024-03-06",
+  },
+]
+
+export default mockUserData;
--- src/views/AuthenticationView/SignInView/index.tsx
+++ src/views/AuthenticationView/SignInView/index.tsx
@@ -13,6 +13,7 @@ import useUserStore from "../../../stores/user.store";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { Cookies, useCookies } from "react-cookie";
+import mockUserData from "../../../mocks/mockUserData";
 
 // useState를 통한 상태관리
 // 회원 아이디: userId
@@ -43,7 +44,7 @@ export default function SignIn() {
 
   // ! useCookie 훅
   // npm install react-cookie
-  const [cookie, setCookies] = useCookies();
+  const [cookies, setCookies] = useCookies();
 
   // 회원의 데이터를 store에서 관리
   const { user, setUser } = useUserStore();
@@ -55,39 +56,52 @@ export default function SignIn() {
       alert("아이디와 비밀번호를 입력하세요.");
       return;
     }
+    // if (userId || userPassword) {
+    //   alert("아이디와 비밀번호를 입력하세요.");
+    //   return;
+    // }
 
-    // 입력한 데이터를 전송하기 위해 data에 담기
-    const data = {
-      userId,
-      userPassword,
-    };
+    //^ mockData를 사용하여 로그인 페이지 구축
+    const foundUser = mockUserData.find(user => user.userId === userId && user.userPassword === userPassword);
 
-    // 로그인 정보(data)를 전송 -post(URL경로, data)
-    axios
-      .post("http://localhost:4080/api/auth/signIn", data)
-      .then((response) => {
-        const responseData = response.data;
-        console.log(responseData);
+    if (!foundUser) {
+      alert('로그인에 실패했습니다.')
+      return
+    }
+    setUser(foundUser)
 
-        // 로그인에 실패할 경우, alert알림
-        if (!responseData.result) {
-          alert("로그인에 실패했습니다.");
-          return;
-        }
+    //* 입력한 데이터를 전송하기 위해 data에 담기
+  //   const data = {
+  //     userId,
+  //     userPassword,
+  //   };
 
-        // 로그인 성공 시, cookies와 회원 data를 설정
-        const { token, exprTime, user } = responseData.data;
-        const expires = new Date();
-        expires.setMilliseconds(expires.getMilliseconds() + exprTime);
+  //*   // 로그인 정보(data)를 전송 -post(URL경로, data)
+  //   axios
+  //     .post("http://localhost:4080/api/auth/signIn", data)
+  //     .then((response) => {
+  //       const responseData = response.data;
+  //       console.log(responseData);
 
-        setCookies("token", token, { expires });
+  //*       // 로그인에 실패할 경우, alert알림
+  //       if (!responseData.result) {
+  //         alert("로그인에 실패했습니다.");
+  //         return;
+  //       }
 
-        setUser(user);
-      })
-      .catch((error) => {
-        alert("로그인에 실패했습니다.");
-      });
-  };
+  //*       // 로그인 성공 시, cookies와 회원 data를 설정
+  //       const { token, exprTime, user } = responseData.data;
+  //       const expires = new Date();
+  //       expires.setMilliseconds(expires.getMilliseconds() + exprTime);
+
+  //       setCookies("token", token, { expires });
+
+  //       setUser(user);
+  //     })
+  //     .catch((error) => {
+  //       alert("로그인에 실패했습니다.");
+  //     });
+   };
 
   return (
     <>
